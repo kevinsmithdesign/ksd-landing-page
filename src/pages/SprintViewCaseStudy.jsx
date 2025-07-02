8725d65ca954d56f21e12602b97bccfaaeb8c904
Kevin Smith <mrkevinsmithdesign@yahoo.com>
2025-07-02 14:54:48 -0500

    Added favicon

---
+++ src/pages/SprintViewCaseStudy.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import {
+  Button,
+  Container,
+  Grid,
+  Card,
+  Typography,
+  Box,
+  Stack,
+  useTheme,
+} from "@mui/material";
+
+const SprintViewCaseStudy = () => {
+  return <Typography sx={{ color: "red" }}>SprintView</Typography>;
+};
+
+export default SprintViewCaseStudy;
